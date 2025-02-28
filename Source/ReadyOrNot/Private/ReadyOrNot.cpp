a349fab849e25c2eb07a3867d941f64f68efdfec
DESKTOP-00LKSEK\MSI <hhs9200@gmail.com>
2025-02-28 18:14:55 +0900

    gitignore 추가

--- Source/ReadyOrNot/Private/ReadyOrNot.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "../Public/ReadyOrNot.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReadyOrNot, "ReadyOrNot" );
-
-DEFINE_LOG_CATEGORY(TPS);
